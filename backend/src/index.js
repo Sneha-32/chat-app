85db586d1a39f3358c5dd70459116e3ee271f1d5
Sneha Sah <sahsneha2062@gmail.com>
2026-06-17 20:51:20 +0545

    updated backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,17 @@
 // const express = require("express");
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
-
+dotenv.config();
 const app= express();
+const PORT = process.env.PORT
 
 app.use("/api/auth",authRoutes);
 
-app.listen(5001, ()=>{
-    console.log("server is running on port http://localhost:5001")
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+    connectDB();
 }) 
+
