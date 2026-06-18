80648778696bc4be3e753d5c57a765809c48324e
Sneha Sah <sahsneha2062@gmail.com>
2026-06-18 18:29:36 +0545

    Token generation

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
+
+
 export const signup = async (req,res) => {
     const {fullName,email,password}=req.body;
     try {
@@ -23,12 +25,20 @@ export const signup = async (req,res) => {
         })
 
         if(newUser){
-
+            generateToken(newUser._id,res);
+            await newUser.save();
+            res.status(201).json({
+                _id:newUser._id,
+                fullName:newUser.fullName,
+                email:newUser.email,
+                profilePic:newUser.profilePic,
+            })
         }else{
             res.status(400).json({message:"Invalid user data"});
         }
     } catch (error) {
-        
+     console.log("Error in signup controller",error.message);
+     res.status(500).json({message:"Internal server error"})   
     }
 };
 
---
+++ backend/src/lib/utils.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken"
+
+// generating a token
+function generateToken(userId,res){
+    const token = jwt.sign({userId},process.env.JWT_SECRET,{
+        expiresIn:"7d"
+    })
+
+    // sending to user in the form of cookie
+    res.cookie("jwt",token,{
+        maxAge: 7*24*60*60*1000,  //milliseconds
+        httpOnly:true,  //prevent XSS attacks cross-site scripting attacks
+        sameSite:"strict",  //CSRF attacks cross-site request forgery attacks
+        secure: process.env.NODE_ENV !== "development"
+    });
+
+    return token;
+}
+
+module.exports={
+    generateToken
+}
