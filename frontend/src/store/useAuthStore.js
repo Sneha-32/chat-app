d3e5b4e22b121706b197c747bdf5bd3b260a339a
Sneha Sah <sahsneha2062@gmail.com>
2026-06-27 22:22:10 +0545

    updated auht store

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -23,6 +23,28 @@ export const useAuthStore = create((set)=>({    //create receives a function
     },
 
     signup: async ({data})=>{
+        set({isSigningUp:true});
+        try {
+            const res = await axiosInstance.post("/auth/signup",data);
+            set({authUser:res.data});
+            toast.success("Account Created Successfully");
+            // now authUser contains the loggid in user's info so that it redirect them away from the login/signup pages
+        } catch (error) {
+            toast.error(error.response.data.message);
 
+        }finally{
+            set({isSigningUp:false});
+        }
+    },
+
+    logout: async()=>{
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({authUser:null});
+            toast.success("logged out successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+            // this help to show the error that may occured in backend
+        }
     }
 }));
