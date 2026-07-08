2530c22062801a27cb4ed17ae4e85ab3582ea932
Sneha Sah <sahsneha2062@gmail.com>
2026-07-08 20:01:46 +0545

    fixed bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,24 @@
 // const express = require("express");
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+
 import { connectDB } from "./lib/db.js";
-import cors from "cors"
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+
 
 dotenv.config();
 const app= express();
 const PORT = process.env.PORT
 
-app.use(express.json());
+// app.use(express.json());
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 
 app.use(cors({
