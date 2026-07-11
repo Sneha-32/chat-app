44c6ca6810fc8bbc269b923bdee59c8596cd6ae3
Sneha Sah <sahsneha2062@gmail.com>
2026-07-11 12:32:28 +0545

    final

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -53,8 +53,12 @@ export const useChatStore = create((set,get)=>({
         }
         const socket = useAuthStore.getState().socket;
 
-        // todo: optimization
+
         socket.on("newMessage",(newMessage)=>{
+            const isMessageSentFromSelectedUser = newMessage.senderId === selectedUser._id;
+            if(!isMessageSentFromSelectedUser){
+                return;
+            }
             set({
                 messages: [...get().messages,newMessage],
             })
